e141bb4016775a9a0f32cb02c5d9122f21b193ff
Guilherme Vicentin <vicentingr@gmail.com>
2023-05-27 10:04:06 -0300

    Revamp tmux and nvim

--- nvim/lua/gvicentin/autocommands.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd(
-    { "BufRead", "BufNewFile" },
-    { 
-        pattern = { "Jenkinsfile", "*.Jenkinsfile", "*.jenkinsfile" },
-        command = "set filetype=groovy" 
-    }
-)
--- nvim/lua/gvicentin/util.lua
+++
@@ -1,5 +0,0 @@
-function Trim()
-    local save = vim.fn.winsaveview()
-    vim.api.nvim_exec("keeppatterns silent! %s/\\s\\+$//e", false)
-    vim.fn.winrestview(save)
-end
