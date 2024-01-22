a5bc2c56287fe840a6d6e5851d452fb6accc9ed1
Guilherme Vicentin <gvicentin@ciandt.com>
2024-01-22 09:54:25 -0300

    Remove unused scripts

--- utils/aws.sh
+++
@@ -1,2 +0,0 @@
-alias aws-ddp-nonprod="saml2aws login --skip-prompt --role arn:aws:iam::272095785328:role/ADFS-Administrators --region us-east-1 --session-duration=28800 --profile ddp-nonprod"
-alias aws-ddp-prod="saml2aws login --skip-prompt --role arn:aws:iam::800143431705:role/ADFS-Administrators --region us-east-1 --session-duration=3600 --profile ddp-prod"
