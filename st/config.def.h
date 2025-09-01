e5d857e00a0e0934bbaf4b54bcc167e91bbfa182
Guilherme Vicentin <vicentingr@gmail.com>
2025-09-01 20:31:38 -0300

    st: patch with st-scrollback-mouse

--- st/config.def.h
+++ st/config.def.h
@@ -176,6 +176,8 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+	{ ShiftMask,            Button4, kscrollup,      {.f = -0.1} },
+	{ ShiftMask,            Button5, kscrolldown,    {.f = -0.1} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
